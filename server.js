d2dc9c708d3b150ae59b2bd527958232495703e3
Risto Vatsar <r.vatsar@gmail.com>
2023-11-13 20:28:15 +0200

    added ejs links

--- server.js
+++ server.js
@@ -4,10 +4,38 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  let movieId = "800158";
+  let url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=95ad7d4eb26fc0b6ce1566fce9ac0b48`;
+
+  axios.get(url).then((response) => {
+    let data = response.data;
+
+    let releaseYear = new Date(data.release_date).getFullYear();
+
+    let genresToDisplay = "";
+    data.genres.forEach((genre) => {
+      genresToDisplay = genresToDisplay + `${genre.name}, `;
+    });
+    let genres = genresToDisplay.slice(0, -2);
+
+    let poster = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${data.poster_path}`;
+
+    let currentYear = new Date().getFullYear();
+
+    res.render("index", {
+      dataToRender: data,
+      currentYear,
+      releaseYear,
+      genres,
+      poster,
+    });
+  });
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
